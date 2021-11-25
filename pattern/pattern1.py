c29d67ba81abb673e3e828151811afaa8f9bd703
Ankan Bhattacharya <ankanbhattacharya89@gmail.com>
2021-11-25 22:56:18 +0530

    pattern1 and 2 solved

---
+++ pattern/pattern1.py
@@ -0,0 +1,3 @@
+numOfRows = int(input("Enter the number of row: "))
+for i in range(0, numOfRows):
+    print("*"*numOfRows)
---
+++ pattern/pattern2.py
@@ -0,0 +1,3 @@
+numOfRows = int(input("Enter the number of rows: "))
+for i in range(1, numOfRows+1):
+    print("*"*i)
